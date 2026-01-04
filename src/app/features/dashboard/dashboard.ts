7d8498538deada20de4fe683de37c0095750366a
vm3do <medoavvadi@gmail.com>
2026-01-04 12:52:13 +0100

    feat: test commit

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,13 +1,16 @@
-import { Component, inject } from '@angular/core';
+import {Component, Inject, inject, signal} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth';
 import { PermissionService } from '../../core/services/permission';
 import { PERMISSIONS } from '../../core/models/permission.model';
 import { CommonModule } from '@angular/common';
+import {SupplierService} from '../../core/services/supplier';
+import {Observable} from 'rxjs';
+import {Stats} from '../stats/stats';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, Stats],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -19,10 +22,30 @@ export class DashboardComponent {
   // Expose permissions to template
   readonly PERMISSIONS = PERMISSIONS;
 
+  private readonly supplierService = inject(SupplierService);
+
+  countElement = signal(0);
+  private loading = signal(false);
+
   logout(): void {
     this.authService.logout();
     this.permissionService.clearCache();
     this.router.navigate(['/auth/login']);
+  }
+
+  ngOnInit() {
+    this.loadSuppliers();
+  }
+
+
+
+  loadSuppliers(): void{
+    this.loading.set(true);
+    this.supplierService.getAll().subscribe((data) => {
+          this.countElement.set(data.length);
+        console.log(this.countElement);
+    })
+
   }
 }
 
--- src/app/features/fournisseurs/fournisseurs-list/fournisseurs-list.ts
+++ src/app/features/fournisseurs/fournisseurs-list/fournisseurs-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import {Component, inject, OnInit, Output, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
