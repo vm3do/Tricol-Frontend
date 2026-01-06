884db8daa2a23162ec34d23092e2ee9990fe5048
vm3do <medoavvadi@gmail.com>
2026-01-06 17:48:55 +0100

    fix: correct dashboard component class name and add logout functionality

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class DashboardComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
+
+
