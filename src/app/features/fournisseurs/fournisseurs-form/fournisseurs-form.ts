ca7ab6e6df6559d2b50e95152efe96e73d496fe8
vm3do <medoavvadi@gmail.com>
2026-01-07 14:25:33 +0100

    feat: add supplier create/edit form with validation

---
+++ src/app/features/fournisseurs/fournisseurs-form/fournisseurs-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fournisseurs-form',
+  imports: [],
+  templateUrl: './fournisseurs-form.html',
+  styleUrl: './fournisseurs-form.css',
+})
+export class FournisseursForm {
+
+}
