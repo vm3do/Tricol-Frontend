5dfd64ce6fd22c27ec14b13f5aa0612ba3546439
vm3do <medoavvadi@gmail.com>
2026-01-25 18:56:02 +0100

    feat: configure app with NgRx store, effects, and HTTP interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 import { jwtInterceptor } from './core/interceptors/jwt-interceptor';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
+import { reducers } from './store';
+import * as authEffects from './store/auth/auth.effects';
+import * as supplierEffects from './store/supplier/supplier.effects';
+import * as productEffects from './store/product/product.effects';
+import * as orderEffects from './store/order/order.effects';
+import * as stockEffects from './store/stock/stock.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,8 +20,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([jwtInterceptor, errorInterceptor])
-    )
-  ]
+    ),
+    provideStore(reducers),
+    provideEffects(authEffects, supplierEffects, productEffects, orderEffects, stockEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+    }),
+  ],
 };
-
-
